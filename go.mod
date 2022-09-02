b9b8b431dc8b0113d0312dd6051f8bd6492a0d49
tiyan-attirmidzi <tiyanattirmidzi20@gmail.com>
2022-09-02 11:16:55 +0700

    add main.go and test module

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/onggolt-dev/mod-test
+
+go 1.18
+
+require github.com/onggolt-dev/go-mod-sample v1.1.0 // indirect
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	say_hello "github.com/onggolt-dev/go-mod-sample"
+)
+
+func main() {
+	fmt.Println(say_hello.SayHello("Tiyan"))
+}
